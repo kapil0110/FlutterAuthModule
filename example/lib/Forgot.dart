86084b85f7dd9e4c4be039bb4de418cc0ab1f305
nimish-thorat <nimish.thorat@pugmarker.com>
2022-02-16 12:33:33 +0530

    In-app routing functionality implemented

--- example/lib/Forgot.dart
+++ example/lib/Forgot.dart
@@ -1,16 +1,21 @@
-import 'package:authentication_package/authentication_package.dart';
-import 'package:flutter/material.dart';
-class Forgot extends StatelessWidget {
-  const Forgot({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ForgotPasswordScreen(
-        logoUrl: "https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/android.svg",
-        onForgotPassword: (){},
-        onLoginLink: () => Navigator.pop(context),
-      ),
-    );
-  }
-}
+// import 'package:authentication_package/authentication_package.dart';
+// import 'package:flutter/material.dart';
+// class Forgot extends StatelessWidget {
+//   const Forgot({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: PMAPForgotPassword(
+//         logoUrl: "https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/android.svg",
+//         onForgotPassword: (data){
+//           print(data);
+//           if(data!.isNotEmpty){
+//             Navigator.pop(context);
+//           }
+//         },
+//         onLoginLink: () => Navigator.pop(context),
+//       ),
+//     );
+//   }
+// }
